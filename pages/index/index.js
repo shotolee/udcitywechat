6494228ddc5e625a086b8580706b1afc859b6fc1
shoto.mac <shotolee@gmail.com>
2019-04-15 00:05:45 +0800

    over 4-8

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,80 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: '',
+    hourlyWeather: []
+  },
+  onPullDownRefresh(){
+    this.getNow(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad(){
+    this.getNow()
+  },
+  getNow(callback){
+   wx.request({
+     url: 'https://test-miniprogram.com/api/weather/now',
+     data: {
+       city: '西安市'
+     },
+     success: res=> {
+       console.log(res)
+       let result = res.data.result
+       let temp = result.now.temp
+       let weather = result.now.weather
+       console.log(temp, weather)
+       this.setData({
+         nowTemp: temp +'°',
+         nowWeather: weatherMap[weather],
+         nowWeatherBackground: '/images/'+ weather + '-bg.png',
+       })
+       wx.setNavigationBarColor({
+         frontColor: '#000000',
+         backgroundColor: weatherColorMap[weather],
+       })
+
+       //set forcast
+       let forecast = result.forecast
+       let hourlyWeather = []
+       let nowHour = new Date().getHours()
+       for (let i = 0; i < 24; i += 3) {
+         hourlyWeather.push({
+           time: (i + nowHour) % 24 + '时',
+           iconPath: '/images/'+ forecast[i/3].weather + '-icon.png',
+           temp: forecast[i/3].temp + '°'
+         })
+       }
+       hourlyWeather[0].time = '现在'
+       this.setData({
+         hourlyWeather: hourlyWeather
+       })
+
+     },
+     complete: ()=>{
+       callback && callback()
+     }
+   })
+  }
+
+})
